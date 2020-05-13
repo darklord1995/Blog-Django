0094a93d8a6bc44044849b2741e8f4b61524c615
“sampathcshetty” <sampath.shetty@rxlogix.com>
2020-05-13 12:34:14 +0530

    Initial commit

---
+++ django_project/blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Post
+import feedparser
+
+def home(request):
+    """
+        Feeds which are to be displayed are extracted from techcrunch.
+        A separate Post model is created where the blogs are stored in DB.
+        
+        - Currently we can add the data in Post from admin page. Functionality for adding 
+          blog from  UI needs to be added.
+    """
+    feeds = feedparser.parse('https://techcrunch.com/feed/')
+    context = {
+        'posts': Post.objects.all(),
+        'feeds' : feeds
+    }
+
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    """
+        Renders about page template when this api is called
+    """
+    return render(request, 'blog/about.html', {'title': 'About'})
